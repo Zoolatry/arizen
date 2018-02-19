6f9a2b2e7b626d2683738a3fe43086856278f75c
Zoolatry <zoolatry@eclipso.eu>
2018-02-19 21:17:05 +0100

    Moved encryption to local package

--- app/renderer/store/index.js
+++ app/renderer/store/index.js
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import path from "path";
-import url from "url";
-import os from "os";
-import fs from "fs-extra";
-import passwordHash from "password-hash";
-import crypto from "crypto";
-import bitcoin from "bitcoinjs-lib";
-import bip32utils from "bip32-utils";
-import zencashjs from "zencashjs";
-import sql from "sql.js";
-import request from "request";
-import fetch from "node-fetch";
+import path from "path"
+import url from "url"
+import os from "os"
+import fs from "fs-extra"
+import passwordHash from "password-hash"
+import crypto from "crypto"
+import bitcoin from "bitcoinjs-lib"
+import bip32utils from "bip32-utils"
+import zencashjs from "zencashjs"
+import sql from "sql.js"
+import request from "request"
+import fetch from "node-fetch"
+import encryption from "encryption"
+import log from "js-vue-logger"
 
 import modules from './modules'
 
@@ -81,6 +83,9 @@ export default new Vuex.Store({
                 state.addresses.push(newAddress)
             }
         },
+        setAddresses (state, list) {
+            state.addresses = list;
+        },
         clearAddress (state) {
             state.addresses = [];
         },
@@ -134,150 +139,72 @@ export default new Vuex.Store({
             // use encrypted json file if exists
             // else try to migrate old SQL-Database
             if(fs.existsSync(jsonPath)) {
-                console.log("Found private key file: " + jsonPath);
+                log.info("Found private key file: " + jsonPath);
 
-                let i = Buffer.byteLength(loginForm.username);
-                let inputBytes = fs.readFileSync(jsonPath);
-                let recoveredLogin = inputBytes.slice(0, i).toString("utf8");
-                let outputBytes = [];
-
-                if (loginForm.username === recoveredLogin) {
-                    let iv = inputBytes.slice(0, i + 64);
-                    i += 64;
-                    let salt = inputBytes.slice(i, i + 64);
-                    i += 64;
-                    let tag = inputBytes.slice(i, i + 16);
-                    i += 16;
-                    let encrypted = inputBytes.slice(i);
-                    let key = crypto.pbkdf2Sync(loginForm.password, salt, 2145, 32, "sha512");
-                    let decipher = crypto.createDecipheriv("aes-256-gcm", key, iv);
-            
-                    decipher.setAuthTag(tag);
-                    outputBytes = decipher.update(encrypted, "binary", "binary");
-                    try {
-                        outputBytes += decipher.final("binary");
-                    } catch (err) {
-                        /*
-                        * Let's hope node.js crypto won't change error messages.
-                        * https://github.com/nodejs/node/blob/ee76f3153b51c60c74e7e4b0882a99f3a3745294/src/node_crypto.cc#L3705
-                        * https://github.com/nodejs/node/blob/ee76f3153b51c60c74e7e4b0882a99f3a3745294/src/node_crypto.cc#L312
-                        */
-                        if (err.message.match(/Unsupported state/)) {
-                            /*
-                            * User should be notified that wallet couldn't be decrypted because of an invalid
-                            * password or because the wallet file is corrupted.
-                            */
-                            outputBytes = [];
-                        } else {
-                            // FIXME: handle other errors
-                            throw err;
-                        }
-                    }
-                }
-
-                if (outputBytes.length > 0) {
-                    var resultJson = outputBytes.toString("utf8");
-                    // console.log(resultJson);
-                    var addresses = JSON.parse(resultJson);
-                    addresses.forEach(item => {
-                        console.log("Found address: " + item.address);
-                        commit('updateAddress', {
+                var result = encryption.readEncryptedObject(jsonPath, this.state.username, this.state.password);
+                if (result != null) {
+                    var addressList = [];
+                    result.forEach(item => {
+                        log.info("Found address: " + item.address + " " + item.name);
+                        addressList.push({
                           pk: item.pk,
                           address: item.address,
-                          balance: item.balance
+                          balance: item.balance,
+                          name: item.name
                         });
                     });
+                    commit('setAddresses', addressList);
                     commit('setLoggedIn', true);
                 }
             }
             else if (fs.existsSync(awdPath)) {
-                console.log("Found obsolete AWD file: " + awdPath);
-                console.log("Trying to migrate to JSON");
+                log.info("Found old AWD file: " + awdPath);
+                log.info("Trying to migrate to JSON");
                 
-                let i = Buffer.byteLength(loginForm.username);
-                let inputBytes = fs.readFileSync(awdPath);
-                let recoveredLogin = inputBytes.slice(0, i).toString("utf8");
-                let outputBytes = [];
+                var result = encryption.readEncryptedBytes(awdPath, this.state.username, this.state.password);
+                if (result != null) {
+                    var addressList = [];
 
-                if (loginForm.username === recoveredLogin) {
-                    let iv = inputBytes.slice(0, i + 64);
-                    i += 64;
-                    let salt = inputBytes.slice(i, i + 64);
-                    i += 64;
-                    let tag = inputBytes.slice(i, i + 16);
-                    i += 16;
-                    let encrypted = inputBytes.slice(i);
-                    let key = crypto.pbkdf2Sync(loginForm.password, salt, 2145, 32, "sha512");
-                    let decipher = crypto.createDecipheriv("aes-256-gcm", key, iv);
-            
-                    decipher.setAuthTag(tag);
-                    outputBytes = decipher.update(encrypted, "binary", "binary");
-                    try {
-                        outputBytes += decipher.final("binary");
-                    } catch (err) {
-                        /*
-                        * Let's hope node.js crypto won't change error messages.
-                        * https://github.com/nodejs/node/blob/ee76f3153b51c60c74e7e4b0882a99f3a3745294/src/node_crypto.cc#L3705
-                        * https://github.com/nodejs/node/blob/ee76f3153b51c60c74e7e4b0882a99f3a3745294/src/node_crypto.cc#L312
-                        */
-                        if (err.message.match(/Unsupported state/)) {
-                            /*
-                            * User should be notified that wallet couldn't be decrypted because of an invalid
-                            * password or because the wallet file is corrupted.
-                            */
-                            outputBytes = [];
-                        } else {
-                            // FIXME: handle other errors
-                            throw err;
+                    var db = new sql.Database(result);
+                    var queryRes = db.exec("select * from wallet");
+                    var rows = [];
+                    queryRes[0].values.map(columns => {
+                        var obj = {};
+                        for (let i = 0; i < columns.length; i++) {
+                            obj[queryRes[0].columns[i]] = columns[i];
                         }
-                    }
-                }
-
-                if (outputBytes.length > 0)
-                {
-                    var db = new sql.Database(outputBytes);
-                    var result = db.exec("select pk, addr from wallet")[0];
-                    result.values.forEach(row => {
-                        var pk = row[0];
-                        var address = row[1];
-                        console.log("Found address: " + address);
-                        commit('updateAddress', {
+                        rows.push(obj);
+                    });
+                    for (let r of rows) {
+                        var pk = r.pk;
+                        var address = r.addr;
+                        var name = "";
+                        if ("name" in r) {
+                            name = r.name;
+                        }
+                        log.info("Found address: " + address + " " + name);
+                        addressList.push({
                             pk: pk,
                             address: address,
-                            balance: 0
+                            balance: 0,
+                            name: name
                         });
-                    });
+                    }
+                    commit('setAddresses', addressList);
                     this.dispatch('saveAddresses');
                     fs.moveSync(awdPath, this.getters.getObsoletePath);
                     commit('setLoggedIn', true);
                 }
             }
             else {
-                console.log("private key file not found: " + jsonPath);
+                log.warn("private key file not found: " + jsonPath);
             }
         },
         saveAddresses({ commit }) {
-            console.log("saving addresses to json file");
+            log.info("saving addresses to json file");
             var jsonPath = this.getters.getPrivateKeyFilePath;
-            var json = JSON.stringify(this.state.addresses);
 
-            // console.log(json);
-            
-            var inputBytes = Buffer.from(json, "utf8");
-            // encrypt
-            let iv = Buffer.concat([Buffer.from(this.state.username, "utf8"), crypto.randomBytes(64)]);
-            let salt = crypto.randomBytes(64);
-            let key = crypto.pbkdf2Sync(this.state.password, salt, 2145, 32, "sha512");
-            let cipher = crypto.createCipheriv("aes-256-gcm", key, iv);
-            let encrypted = Buffer.concat([cipher.update(inputBytes), cipher.final()]);
-
-            var data = Buffer.concat([iv, salt, cipher.getAuthTag(), encrypted]);
-
-            fs.writeFileSync(jsonPath, data, function (err) {
-                if (err) {
-                    return console.log(err);
-                }
-            });
+            encryption.writeObjectEncrypted(this.state.addresses, jsonPath, this.state.username, this.state.password);
         },
         logout({ commit }) {
             console.log("logging out");
